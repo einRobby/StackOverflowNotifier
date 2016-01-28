a669bf1dd92916c4fc2d2111dd6a419322fa5615
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2016-01-28 23:34:05 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Windows/StackOverflowNotifier.UWP/Package.appxmanifest
    #	Windows/StackOverflowNotifier.UWP/Properties/AssemblyInfo.cs
    #	Windows/StackOverflowNotifier.UWP/StackOverflowNotifier.UWP.csproj

--- Windows/StackOverflowNotifier.UWP/Properties/AssemblyInfo.cs
+++ Windows/StackOverflowNotifier.UWP/Properties/AssemblyInfo.cs
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Stack Overflow Notifier")]
@@ -18,43 +17,13 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-=======
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Stack Overflow Notifier")]
-[assembly: AssemblyDescription("Notifier for incoming and unanswered Stack Overflow questions that matches your selection of tags.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Thepagedot")]
-[assembly: AssemblyProduct("Stack Overflow Notifier")]
-[assembly: AssemblyCopyright("Copyright © Thepagedot 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.1.0.0")]
->>>>>>> origin/master
 [assembly: ComVisible(false)]

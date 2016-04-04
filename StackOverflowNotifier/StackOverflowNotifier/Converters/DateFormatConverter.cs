5e6ff8a861f71df7552d78d6fdc53a23bb35f460
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2016-04-04 21:27:50 +0200

    Added converter

---
+++ StackOverflowNotifier/StackOverflowNotifier/Converters/DateFormatConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StackOverflowNotifier.Converters
+{
+    public sealed class DateFormatConverter : IValueConverter
+    {
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (!(value is DateTime))
+				return value;
+
+			return ((DateTime)value).ToString("f");
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+    }
+}

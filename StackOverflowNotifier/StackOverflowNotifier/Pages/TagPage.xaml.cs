a27af019d54134bc2501ee4e49245b7c8cefcd75
Robin-Manuel Thiel <robin-manuelthiel@robinmanuelsmbp.guest.corp.microsoft.com>
2016-04-16 17:58:41 +0200

    Added settings icon to tag page

--- StackOverflowNotifier/StackOverflowNotifier/Pages/TagPage.xaml.cs
+++ StackOverflowNotifier/StackOverflowNotifier/Pages/TagPage.xaml.cs
@@ -11,6 +11,12 @@ namespace StackOverflowNotifier
 		{
 			InitializeComponent();
 
+			// On Android, add the settings button to the toolbar. On iOS it has been added to the tab bar
+			if (Device.OS == TargetPlatform.Android)
+			{
+				ToolbarItems.Add(new ToolbarItem("Settings", "Settings.png", new Action(() => { App.Bootstrapper.MainViewModel.NavigateToSettingsCommand.Execute(null); }), ToolbarItemOrder.Primary, 0));
+			}
+
 			// Add icon to tab on iOS
 			if (Device.OS == TargetPlatform.iOS)
 			{
